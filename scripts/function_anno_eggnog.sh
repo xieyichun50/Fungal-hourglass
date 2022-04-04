fc30b006b0e29f8aec35b1a7335c944b2ea850e7
Yichun <59522685+xieyichun50@users.noreply.github.com>
2022-04-04 16:52:07 +0800

    Create function_anno_eggnog.sh

---
+++ scripts/function_anno_eggnog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+emapper.py  --target_orthologs all --go_evidence non-electronic -m diamond --pident 40 --seed_ortholog_evalue 0.001 --seed_ortholog_score 60 --query_cover 20 --subject_cover 20 --override -i Athaliana_167_protein.fa -o Arabidopsis_thaliana.protein.fa.eggnog --output_dir eggnog/ --temp_dir eggnog_tmp/ --database none --cpu 138 --target_taxa 3699
+
+emapper.py  --target_orthologs all --go_evidence non-electronic -m diamond --pident 40 --seed_ortholog_evalue 0.001 --seed_ortholog_score 60 --query_cover 20 --subject_cover 20 --override -i Coprinopsis_cinerea.proteins.fa -o Coprinopsis_cinerea.proteins.fa.eggnog --output_dir eggnog/ --temp_dir eggnog_tmp/ --database none --cpu 138 --target_taxa 4751
+
+emapper.py  --target_orthologs all --go_evidence non-electronic -m diamond --pident 40 --seed_ortholog_evalue 0.001 --seed_ortholog_score 60 --query_cover 20 --subject_cover 20 --override -i Danio_rerio.proteins.fa -o Danio_rerio.proteins.fa.eggnog --output_dir eggnog/ --temp_dir eggnog_tmp/ --database none --cpu 138 --target_taxa 7898
+
+emapper.py  --target_orthologs all --go_evidence non-electronic -m diamond --pident 40 --seed_ortholog_evalue 0.001 --seed_ortholog_score 60 --query_cover 20 --subject_cover 20 --override -i Drosophila_melanogaster.proteins.fa -o Drosophila_melanogaster.proteins.fa.eggnog --output_dir eggnog/ --temp_dir eggnog_tmp/ --database none --cpu 138 --target_taxa 7214
+
+emapper.py  --target_orthologs all --go_evidence non-electronic -m diamond --pident 40 --seed_ortholog_evalue 0.001 --seed_ortholog_score 60 --query_cover 20 --subject_cover 20 --override -i Fusarium_graminearum.protein.fa -o Fusarium_graminearum.protein.fa.eggnog --output_dir eggnog/ --temp_dir eggnog_tmp/ --database none --cpu 138 --target_taxa 4751
+
+emapper.py  --target_orthologs all --go_evidence non-electronic -m diamond --pident 40 --seed_ortholog_evalue 0.001 --seed_ortholog_score 60 --query_cover 20 --subject_cover 20 --override -i Rhizopus_delemar.protein.fa -o Rhizopus_delemar.protein.fa.eggnog --output_dir eggnog/ --temp_dir eggnog_tmp/ --database none --cpu 138 --target_taxa 4751
